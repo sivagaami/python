dce0ddc3692f3f8d07bd396a7649556f7ed8dc4e
sivagaami <47942779+sivagaami@users.noreply.github.com>
2022-02-28 13:18:29 +0530

    Update Ex4.py

--- Assessment 4/Ex4.py
+++ Assessment 4/Ex4.py
@@ -1,12 +1,12 @@
 import pandas as pd
-ddf = pd.read_csv('C:\\Users\\Admin\\Desktop\\j\\matches.csv')
+Matches_played = pd.read_csv('C:\\Users\\Admin\\Desktop\\j\\matches.csv')
 
-ddf.head()
+Matches_played.head()
 
 #to print columns
-ddf.columns
+Matches_played.columns
 
-f=ddf['win_by_wickets'].loc[ddf['win_by_wickets']!=0]
+f=Matches_played['win_by_wickets'].loc[ddf['win_by_wickets']!=0]
 print('f')
 
 #mean
@@ -16,7 +16,17 @@ f.mean()
 f.std()
 
 #group by
-grouped__df = ddf.groupby(["city", "winner"])
-for key,item in grouped__df:
-  a_group = grouped__df.get_group(key)
-  print(a_group, "\n")
+group = Matches_played.groupby(["city", "winner"])
+for key,item in group:
+  df_group = group.get_group(key)
+  print(df_group, "\n")
+  
+ # Conditional Probability
+Total_matches =(Matches_played['city'].value_counts()['Mumbai'])
+Total_matches
+
+Mumbai =(Matches_played['winner'].value_counts()['Mumbai Indians'])
+Mumbai
+
+probability = Mumbai / Total_matches
+probability
